aa7f396a3e8ba65de604f1d8129c193695b47257
the server <106686224+Mester-Root@users.noreply.github.com>
2022-06-30 16:09:02 +0430

    Add files via upload

---
+++ cracker-blogex.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#type file > [panel]
+def method():
+    import os,time
+    method = input ('''
+\n\033[36m
+[📌] method for cracker blogex\033[31m [blogex.ir] \033[92m:>
+
+\033[31m[*] \033[20;37mfor UTF-8 =  \033[93m [1]
+
+\033[31m[*] \033[20;37mfor android = \033[93m[2]
+
+\033[31m[type] \033[36mnumber method's \033[20;37m==[>] \033[00;37m''')
+    print ('\n\n')
+    if method == '1':
+        os.system("python blogex.py")
+    elif method == '2':
+        os.system("python blogx.py")
+    else:
+        time.sleep(1)
+        print ("\n\033[31m[!] > \033[35mNOT FOUND NUMBER FOR METHOD \033[20;37m| try again [♻️]")
+        time.sleep(3)
+        method()
+method()
+# the meno or panel for cracker :D
